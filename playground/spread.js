2b24c19148bc78f64c910672a80f0a5ef9505c02
The Thai <thethai.nguyen.212@gmail.com>
2017-06-12 15:12:13 +0700

    Add playground/spread operator

---
+++ playground/spread.js
@@ -0,0 +1,36 @@
+// function add(a,b){
+//   return a+b;
+// }
+//
+// console.log(add(3,1));
+//
+// var toAdd = [9,5];
+// console.log(add(...toAdd));
+//
+//
+// var groupA = ['a','b'];
+// var groupB = ['c'];
+// var final = [...groupB,1,...groupA];
+//
+// console.log(final);
+//
+//
+// var person1 = ['thethai',20];
+// var person2 = ['thaithe',26];
+//
+// function greet (name,age){
+//   console.log('Hi '+name+' you are '+age);
+//   console.log('hi',...person1);
+//
+// }
+//
+// greet(...person1);
+// greet(...person2);
+//
+//
+var names = ['mike','ben'];
+var final = ['andrew',...names];
+
+final.forEach(function (a){
+  console.log('Hi',a);
+});
